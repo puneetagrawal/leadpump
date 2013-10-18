09f0c6e213da85439b4ce8e1ed00dbb5a5062dc4
root <root@ubuntu.(none)>
2013-10-18 06:34:57 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leadpump::Application.config.secret_token = 'f67695cce64fec58311b277d5cfab01efc605c77adc2cc984ccd7dec7822a02353fe88d896bbf227d83a271b861319b329ae231b324ba309f7fafc2bc73bb64f'
---
+++ config/initializers/stripe.rb
@@ -0,0 +1,2 @@
+Stripe.api_key  = "sk_test_MDqPbXEEDmOnzrqwJhyzmYeF"
+STRIPE_PUBLIC_KEY = "pk_test_khzNUZcEtnlUCiom3lA2nQtI"
