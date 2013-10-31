4ca2a9c01957e2f842c746cffeda6a23afe39cf8
root <root@ubuntu.(none)>
2013-10-31 20:44:57 +0530

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-DiscountsOnLocation.create(locationRanges: '1-10', discountPercentage: 5, chargePerUser:"51,21,31,51,81")
-DiscountsOnLocation.create(locationRanges: '11-20', discountPercentage: 10, chargePerUser:"47,17,27,47,77")
-DiscountsOnLocation.create(locationRanges: '21-50', discountPercentage: 15, chargePerUser:"41,11,21,41,71")
-DiscountsOnLocation.create(locationRanges: '51-100', discountPercentage: 20, chargePerUser:"37,7,17,37,67")
+DiscountsOnLocation.create(locationRanges: '1-10', discountPercentage: 5, chargePerUser:"21,31,51,81")
+DiscountsOnLocation.create(locationRanges: '11-20', discountPercentage: 10, chargePerUser:"17,27,47,77")
+DiscountsOnLocation.create(locationRanges: '21-50', discountPercentage: 15, chargePerUser:"11,21,41,71")
+DiscountsOnLocation.create(locationRanges: '51-100', discountPercentage: 20, chargePerUser:"7,17,37,67")
+DiscountsOnLocation.create(locationRanges: '101-200', discountPercentage: 20, chargePerUser:"5,15,35,65")
 
 
 DiscountsOnPeriod.create(periodType:'monthly', discountPercentage:0)
-DiscountsOnPeriod.create(periodType:'yearly', discountPercentage:10)
+DiscountsOnPeriod.create(periodType:'yearly', discountPercentage:17)
 
 Plan.create(name:"Basic",price:21, user_position:"Contact Manager", number_of_user:20, lead_management:"250",apportunity_dashboard:true,team_management:false,daily_sales_report:false,daily_sales_projection:false,full_dashboard_enabled:false,traditional_refferals:false,leadpump_social_inviter:false,socail_refferals:"",online_mall:false,daily_team_usage_report:false,unlimited_team_training:false,unlimited_support:false,national_spokeswoman:false)
 Plan.create(name:"Advanced",price:31, user_position:"Manage a team-track sales", number_of_user:20, lead_management:"250",apportunity_dashboard:true,team_management:true,daily_sales_report:true,daily_sales_projection:true,full_dashboard_enabled:false,traditional_refferals:false,leadpump_social_inviter:false,socail_refferals:"",online_mall:false,daily_team_usage_report:false,unlimited_team_training:false,unlimited_support:false,national_spokeswoman:false)
