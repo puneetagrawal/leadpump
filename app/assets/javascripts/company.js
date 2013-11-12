31df904a3a076ed61c3093198fdc7f290ce612bb
root <root@ubuntu.(none)>
2013-11-13 00:12:12 +0530

    modify css

--- app/assets/javascripts/company.js
+++ app/assets/javascripts/company.js
@@ -4,26 +4,37 @@ $(document).ready(function(){
 
 function initCompanyCreateOrUpdate(){
 	$(".userEdit").click(function(){
-		$('.formfields').html('<img src="/assets/ajax-loader.gif" style="margin:165px;float:left;">')
-		id = $(this).closest('tr').attr('id');
-		userId = id.split("_")[1]
-		url = '/company/'+userId+'/edit'
-		$.get(url, {}, function (data) {
-			$(".headmsg").text("Update User");
-			$(".submitUserBtn").html('<input type="button" class="btn yellow compSubmit" value="Update User">');	
-			initCompanyCreateOrUpdate();			
-		});
+		companyEdit(this);
 	});
 
 	$(".compSubmit").click(function(){
-		btnHtml = $(this).parent().html();
-		$(this).parent().html('<img src="/assets/ajax-loader.gif" style="margin-left: 40px;>');
-		id = $("#user_id").val();
-		url = '/update/'+id+'';
-		$.get(url, {inputs:formfields()}, function (data) {
-			$('.submitUserBtn').html(btnHtml);	
-			initCompanyCreateOrUpdate();
-		});
+		alert(">>>>>>>");
+		companySubmit(this);
 	});		
+}
+
+function companyEdit(obj){
+	$('.formfields').html('<img src="/assets/ajax-loader.gif" style="margin:165px;float:left;">')
+	id = $(obj).closest('tr').attr('id');
+	userId = id.split("_")[1]
+	url = '/company/'+userId+'/edit'
+	$.get(url, {}, function (data) {
+		$(".headmsg").text("Update User");
+		$(".submitUserBtn").html('<input type="button" class="btn yellow compSubmit" value="Update User">');	
+		initCompanyCreateOrUpdate();
+		initialization();			
+	});
+}
+
+function companySubmit(obj){
+	btnHtml = $(obj).parent().html();
+	$(obj).parent().html('<img src="/assets/ajax-loader.gif" style="margin-left: 40px;>');
+	id = $("#user_id").val();
+	url = '/company/update/'+id+'';
+	$.get(url, {inputs:formfields()}, function (data) {
+		$('.submitUserBtn').html(btnHtml);	
+		initCompanyCreateOrUpdate();
+		initialization();
+	});
 }
 
--- app/assets/javascripts/lead.js
+++ app/assets/javascripts/lead.js
@@ -1,27 +1,62 @@
+
 $(document).ready(function(){
-	initLeadCreateOrUpdate();
+	$(".leadFilterByName").change(function(){
+		if($(this).val() != ''){
+			leadFilterByName($(this).val())	
+		}		
+	});	
+	initLeadCreateOrUpdate();	
 });
 
 function initLeadCreateOrUpdate(){
 	$(".leadEdit").click(function(){
-		$('.formfields').html('<img src="/assets/ajax-loader.gif" style="margin:165px;float:left;">')
-		id = $(this).closest('tr').attr('id');
-		leadId = id.split("_")[1]
-		url = '/leads/'+leadId+'/edit'
-		$.get(url, {}, function (data) {
-			$(".headmsg").text("Update Lead");
-			$(".submitLeadBtn").html('<input type="button" class="btn yellow leadSubmit" value="Update Lead">');	
-			initLeadCreateOrUpdate();			
-		});
+		leadEdit(this);
 	});
 
 	$(".leadSubmit").click(function(){
-		$(this).parent().html('<img src="/assets/ajax-loader.gif" style="margin-left: 40px;>');
-		id = $("#lead_id").val();
-		url = '/update/'+id+'';
-		$.get(url, {inputs:formfields()}, function (data) {
-			initLeadCreateOrUpdate();
-		});
+		leadSubmit(this);
 	});		
+	initLeadActiveSelect();
+	initialization();
 }
 
+function leadEdit(obj){
+	$('.formfields').html('<img src="/assets/ajax-loader.gif" style="margin:165px;float:left;">')
+	id = $(obj).closest('tr').attr('id');
+	leadId = id.split("_")[1];
+	url = '/leads/'+leadId+'/edit'
+	$.get(url, {}, function (data) {
+		$(".headmsg").text("Update Lead");
+		$(".submitLeadBtn").html('<input type="button" class="btn yellow leadSubmit" value="Update Lead">');	
+		initLeadCreateOrUpdate();
+	});
+	
+}
+
+function leadSubmit(obj){
+	$(".submitLeadBtn").html('<img src="/assets/ajax-loader.gif" style="margin-left: 40px;">');
+	id = $("#lead_id").val();
+	url = '/update/'+id+'';
+	$.get(url, {inputs:formfields()}, function (data) {
+		initLeadCreateOrUpdate();
+		showSuccessMsg("Lead Updated Successfully");
+	});
+	
+}
+
+function leadFilterByName(userId){
+	$("#leadListContent").html('<img src="/assets/ajax-loader.gif" style="margin:100px 350px 300px">')
+	url = '/leads/filterbyname';
+	$.post(url, {userId:userId}, function (data) {	
+		initialization();
+		initLeadCreateOrUpdate();
+	});
+}
+
+function leadSearchFilter(leadId){
+	url = '/leads/leadsearchfilter';
+	$.get(url, {leadId:leadId}, function (data) {	
+		initialization();
+		initLeadCreateOrUpdate();
+	});
+}
