5242aadc853fe3cee4b2d4e714bef3b92dfbf81c
root <root@leadpump2.(none)>
2013-11-15 09:44:47 +0000

    Server changes in application

--- config/application.rb
+++ config/application.rb
@@ -69,8 +69,8 @@ module Leadpump
         domain: "gmail.com",
         authentication: "plain",
         enable_starttls_auto: true,
-        user_name: "testatdev@gmail.com",
-        password: "test@dev"
+        user_name: "webteam786@gmail.com",
+        password: "webteam786"
       } 
     end
 end
