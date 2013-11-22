6fe1877499ddd6778aa611ab481b10e3521b174b
vishwnath <vishwnath.yadav@ongraph.com>
2013-11-22 21:06:52 +0530

    index file changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,14 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   protect_from_forgery
 
-  #   def after_sign_in_path_for(resource)
-  #     if current_user.role_id.eql?"1"
-  # 	  admin_index_path
-  # 	 else
-  # 		root_url
-  # 	 end
-  # end
+    def after_sign_in_path_for(resource)
+      if current_user.role_id == 1
+  	  	url = admin_index_path
+  	 else
+  		url = root_url
+  	 end
+  	 return url
+  end
 
  
 end
