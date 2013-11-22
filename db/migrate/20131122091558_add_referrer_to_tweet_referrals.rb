4a9b74c6fa069b7542ae3c98bc694fafd5db9fc1
vishwnath <vishwnath.yadav@ongraph.com>
2013-11-22 20:46:04 +0530

    admin dashboard

--- db/migrate/20131122091558_add_referrer_to_tweet_referrals.rb
+++ db/migrate/20131122091558_add_referrer_to_tweet_referrals.rb
@@ -1,5 +1,5 @@
 class AddReferrerToTweetReferrals < ActiveRecord::Migration
   def change
-    add_column :tweet_referrals, :referrer, :integer
+    #add_column :tweet_referrals, :referrer, :integer
   end
 end
--- db/migrate/20131122094635_change_data_type_for_tweet_referrals.rb
+++ db/migrate/20131122094635_change_data_type_for_tweet_referrals.rb
@@ -1,9 +1,9 @@
 class ChangeDataTypeForTweetReferrals < ActiveRecord::Migration
   def up
-  	change_column :tweet_referrals, :referrer, :integer
+  	#change_column :tweet_referrals, :referrer, :integer
   end
 
   def down
-  	change_column :tweet_referrals, :referrer, :integer
+  	#change_column :tweet_referrals, :referrer, :integer
   end
 end
