3126a2bc882de3ec8c88ccbc8ac0aca385e99e4e
manisha <manisha.joshi@ongraph.com>
2013-12-02 19:11:43 +0530

    changes done

--- app/controllers/onlinemall_controller.rb
+++ app/controllers/onlinemall_controller.rb
@@ -1,8 +1,21 @@
 class OnlinemallController < ApplicationController
   def index
+  	@onlinemalls = Onlinemall.all
   	@onlinemall = Onlinemall.new
   end
 
   def create
+  	@onlinemall = Onlinemall.new(:title=>params[:onlinemall][:title], :link=>params[:onlinemall][:link],:user_id=>current_user.id)
+  	mallpic = Mallpic.new(:avatar=>params[:onlinemall][:mallpic][:avatar])
+  	if onlinemall.valid? && mallpic.valid?
+  		onlinemall.save
+  		mallpic.onlinemall_id = onlinemall.id
+  		mallpic.save
+	else
+		render "index"
+	end
+	flash[:notice] = "Mall item added successfully"
+	redirect_to onlinemall_index_path
   end
-end
+
+end #main
--- app/models/onlinemall.rb
+++ app/models/onlinemall.rb
@@ -1,7 +1,7 @@
 class Onlinemall < ActiveRecord::Base
   attr_accessible :active, :link, :mallpic_id, :title, :user_id
 
-  has_many :mallpics
+  has_one :mallpics
   belongs_to :user
 
   accepts_nested_attributes_for :mallpics
