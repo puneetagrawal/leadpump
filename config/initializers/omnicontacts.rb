9962224bbe9ed9097248cb5be6f26ca30930ea1f
vishwnath <vishwnath.yadav@ongraph.com>
2013-12-09 12:09:27 +0530

    hotmail integration

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -5,4 +5,5 @@ Rails.application.middleware.use OmniContacts::Builder do
   importer :yahoo, "dj0yJmk9Sjg0TTY3SWx4TUpHJmQ9WVdrOWNYSkxjbHB5TjJVbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0wYw--", "25a75406ff23f30a0e537004bd46cecb31a35277", {:callback_path => '/callback'}
   # importer :hotmail, "0000000040110A07", "-1aNOxtIYeSotxXR3BwLwG8e9zjgKF-z", {:redirect_path => "/oauth2callback"}
   # importer :facebook, "client_id", "client_secret"
+  importer :hotmail, "000000004810E275", "crEyWbhPXnOujsALez0rqU7q7-YHBaU7", {:callback_path => '/callback'}
 end
