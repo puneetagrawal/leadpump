d2d5964599c62ce480f352252d13ed65bf181749
vishwnath <vishwnath.yadav@ongraph.com>
2013-12-24 12:55:06 +0530

    dashboard loader image added

--- app/assets/javascripts/seeusergauge.js
+++ app/assets/javascripts/seeusergauge.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 	$('.seeusergauge ul li').click(function(){
+		  $(this).find('.pull-left').append('<img class="seeuserload" src="/assets/small-load.gif" style="margin-left:30%" />');
 		  id = $(this).attr('id').split("_")[1];
 		  url = '/viewusergauge';
-		  $.get(url, {id:id}, function (data) {			
+		  $.get(url, {id:id}, function (data) {	
+		  $('.seeuserload').remove();		
 		  });
 	}); 
 });
