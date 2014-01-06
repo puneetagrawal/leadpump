c87535e1b01a22820b65378d5a32242afbeef7f9
vishwnath <vishwnath.yadav@ongraph.com>
2014-01-06 13:28:21 +0530

    sales data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -154,10 +154,10 @@ end
   end
 
   def pass
-    # user = User.find(params[:id])
-    # company = user.fetchCompany
-    # landpage = LandingPage.where(:user_id=>company.id).last
-    # @dayscount = landpage.present? ? landpage.no_of_days.present? ? landpage.no_of_days : 1 : 1
+    user = User.find(params[:id])
+    company = user.fetchCompany
+    landpage = LandingPage.where(:user_id=>company.id).last
+    @dayscount = landpage.present? ? landpage.no_of_days.present? ? landpage.no_of_days : 1 : 1
   end
 
   def print_pass
