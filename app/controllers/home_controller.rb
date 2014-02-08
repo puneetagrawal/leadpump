db3e2d89c9faaf4dec7b852c6fe3010dfba648b7
vishwnath <vishwnath.yadav@ongraph.com>
2014-02-08 07:15:25 +0530

    ddd

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -166,8 +166,8 @@ end
 
   def pass
     #@company = User.find(2)
-    #Company.removeAllPrintPassSessions(session)
-    user = User.find(2)
+    Company.removeAllPrintPassSessions(session)
+    user = User.find(params[:id])
     @company = user.fetchCompany
     landpage = LandingPage.where(:user_id=>@company.id).last
     @dayscount = landpage.present? ? landpage.no_of_days.present? ? landpage.no_of_days : 1 : 1
