2b4834b65aeb2339794b7f864b37b973c271ec13
vishwnath <vishwnath.yadav@ongraph.com>
2014-04-05 19:27:12 +0530

    schedular time change

--- config/initializers/task_sheduler.rb
+++ config/initializers/task_sheduler.rb
@@ -5,6 +5,6 @@ scheduler = Rufus::Scheduler.new
 scheduler.at "00:30:00" do
 	User.create_charge_for_trail_user
 end
-scheduler.at "12:08:00" do
+scheduler.at "02:30:00" do
 	AutoResponderRecord.send_auto_respond_mail
 end
