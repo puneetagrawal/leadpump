e0a0b863575e5b9875f6fdf2bee7d86a49164ede
unknown <vishwnath@.(none)>
2014-04-08 01:23:13 -0700

    auto responder

--- app/models/auto_responder_record.rb
+++ app/models/auto_responder_record.rb
@@ -5,7 +5,6 @@ class AutoResponderRecord < ActiveRecord::Base
   def self.save_respond_message(user_lead, user)
   	respond_message = AutoResponder.where(:user_id=>user)
   	if respond_message.present?
-  		logger.debug(respond_message.size)
   		respond_message.each do|res|
   			if res.message.present? && res.day.present? && res.subject.present?
   				reps = AutoResponderRecord.new(:auto_responder_id=>res.id, :user_lead_id=>user_lead.id, :respond_date=> Date.today + res.day, :mail_sent=>false)
@@ -19,14 +18,12 @@ class AutoResponderRecord < ActiveRecord::Base
   	end
   end
   def self.send_auto_respond_mail
-  	logger.debug("dfssssssssssssssssss")
   	mailer_list = AutoResponderRecord.where(:respond_date => Date.today)
-  	logger.debug("DSFSDFSFSDFDFFD")
   	if mailer_list.present?
   		mailer_list.each do|ml| 
   			ul = UserLeads.find(ml.user_lead_id)
   			company = ul.user.fetchCompany
-			au = AutoResponder.find(ml.auto_responder_id)
+			  au = AutoResponder.find(ml.auto_responder_id)
   			if ul.lead.email.present? 
   				Emailer.send_respond_mail(ul.lead.email, au.message.html_safe, au.subject, company.name, company.email).deliver
   			end
