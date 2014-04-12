865572642174119fa6e241613ae1a0c88f463591
unknown <vishwnath@.(none)>
2014-04-12 17:40:25 +0530

    origin master

--- app/models/user.rb
+++ app/models/user.rb
@@ -333,19 +333,6 @@ def self.find_user(id)
   end
 end
 
-def self.create_charge_for_trail_user
-  logger.debug("Creating charge for trial users --- #{Date.today}")
-  trial_user = Subscription.where("expiry_date < ?",Date.today)
-  if trial_user.present?
-    logger.debug(trial_user.size)
-    logger.debug("ddddddddddddddddddddddddddddd")
-    trial_user.each do |sub|
-      logger.debug("paying for user #{sub.id}")
-      User.create_charge(sub)
-    end
-  end
-end
-
 def self.create_charge(sub)
   user = sub.user
   if user.present? && user.role_id == 2 && sub.customer_id.present?
--- config/initializers/task_sheduler.rb
+++ config/initializers/task_sheduler.rb
@@ -2,9 +2,11 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.at "00:30:00" do
-	User.create_charge_for_trail_user
-end
-scheduler.at "02:30:00" do
+scheduler.at "00:05:00" do
 	AutoResponderRecord.send_auto_respond_mail
+end
+
+
+scheduler.at "01:30:00" do
+	AutoResponder.create_charge_for_trail_user
 end
