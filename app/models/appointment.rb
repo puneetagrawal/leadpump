5f1a0fc7880ab48d1c1881dff1826ff53d8a396b
unknown <vishwnath@.(none)>
2014-04-25 21:35:54 +0530

    d

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -44,9 +44,9 @@ class Appointment < ActiveRecord::Base
         users = Company.where(:company_admin_id => user.id).pluck(:company_user_id)
         users << user.id
         users = users.present? ? users.uniq : []
-        appointments = Appointment.includes(:lead).includes(:user).where(:user_id=>users).where("app_date_time > ? and app_date_time < ?", date,date1)
+        appointments = Appointment.includes(:lead).includes(:user).where(:user_id=>users).where("app_date_time > '"+ date +"' and app_date_time < '"+ date1+"'")
       when :employee
-        appointments = Appointment.includes(:lead).includes(:user).where(:user_id=>user.id).where("app_date_time > ? and app_date_time < ?", date,date1)
+        appointments = Appointment.includes(:lead).includes(:user).where(:user_id=>user.id).where("app_date_time > '"+ date +"' and app_date_time < '"+ date1 +"'")
     end
     return appointments
   end
