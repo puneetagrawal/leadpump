4c29ba59e3a9ebaf0e7d9afc7fe9e693d916b31f
vishwnath <vishwnath.yadav@ongraph.com>
2014-05-06 10:33:02 -0700

    unicorn rb change

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,10 @@
 root = "/root/leadpump"
-
 working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
+pid "#{root}/tmp/pids/unicorn_leadpump.pid"
+listen "/tmp/unicorn.leadpump.sock"
+
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
-#listen "127.0.0.1:8080"
-listen "/tmp/unicorn.leadpump.sock"
+
 worker_processes 2
 timeout 30
