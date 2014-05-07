2532c5725dac0a42184e57c309ec46d725bf95c3
unknown <vishwnath@.(none)>
2014-05-07 07:54:20 -0700

    dd

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -6,5 +6,5 @@ Rails.application.middleware.use OmniContacts::Builder do
   importer :yahoo, YAHOO_SECRET, YAHOO_KEY
   #importer :live, "0000000040112520", "X5fYoftJYclFsID77zl3mt-HGYkEdCvz"
   # importer :facebook, "client_id", "client_secret"
-  importer :hotmail, "0000000048110A7E", "cdPc2D3VzL49S2xjIUtmfDR3txQnIawN"
+  importer :hotmail, "0000000044119D8D", "guONzW6a0U1QR70sTxv4riJw0zvh4W-s"
 end
