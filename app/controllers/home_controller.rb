8a74c731a8a48125c6ee789f127f026b7379172b
unknown <deepak.kiran@ongraph.com>
2014-05-11 14:48:44 +0530

    d

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -452,7 +452,7 @@ def calculateAmount
         logger.debug("subscription saving")
          address = Address.find_by_user_id("#{@user.id}")
           logger.debug("address found")
-         #Emailer.send_user_info_to_admin(@user, params[:user_ip], address).deliver
+         Emailer.send_user_info_to_admin(@user, params[:user_ip], address).deliver
           logger.debug("email sended")
          sign_in :user, @user
         end
